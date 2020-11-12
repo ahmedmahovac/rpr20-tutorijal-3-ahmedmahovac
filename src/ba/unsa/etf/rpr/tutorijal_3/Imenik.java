3a2befa3050289a94cc8533f7bbf31451461a08f
ahmedmahovac <amahovac1@etf.unsa.ba>
2020-11-12 21:56:03 +0100

    dodana metoda dajBroj u klasi imenik

--- src/ba/unsa/etf/rpr/tutorijal_3/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal_3/Imenik.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.tutorijal_3;
 
+import java.util.HashMap;
+
 public class Imenik {
+  private HashMap<String, TelefonskiBroj> brojevi;
+
+
+  public  String dajBroj(String ime) {
+      return brojevi.get(ime).ispisi();
+  }
+
 }
