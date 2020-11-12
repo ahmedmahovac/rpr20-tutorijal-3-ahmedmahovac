70dd1b2f86fdd224aa7ebb68b3513456661756f1
ahmedmahovac <amahovac1@etf.unsa.ba>
2020-11-12 21:53:13 +0100

    dodan konstruktor klasi medunarodniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal_3/MedunarodniBroj.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.tutorijal_3;
+
+public class MedunarodniBroj extends TelefonskiBroj{
+    private String medunarodniBroj;
+    private Grad grad;
+
+    public MedunarodniBroj(String medunarodniBroj, Grad grad) {
+        this.medunarodniBroj = medunarodniBroj;
+        this.grad = grad;
+    }
+
+}
