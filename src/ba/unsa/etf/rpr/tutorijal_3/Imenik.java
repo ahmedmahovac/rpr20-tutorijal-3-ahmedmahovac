574c91ef88d878db598260b7e7fe99dbe84795d6
ahmedmahovac <amahovac1@etf.unsa.ba>
2020-11-12 22:06:00 +0100

    implementirane metode dajIme, dajBroj i dodaj u metodi Imenik

--- src/ba/unsa/etf/rpr/tutorijal_3/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal_3/Imenik.java
@@ -1,13 +1,25 @@
 package ba.unsa.etf.rpr.tutorijal_3;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class Imenik {
   private HashMap<String, TelefonskiBroj> brojevi;
 
 
+    void dodaj(String ime, TelefonskiBroj broj) {
+        brojevi.put(ime, broj);
+    }
   public  String dajBroj(String ime) {
       return brojevi.get(ime).ispisi();
   }
+
+    public String dajIme(TelefonskiBroj broj) {
+        for(String tmp : brojevi.keySet())  if(brojevi.get(tmp).equals(broj)) return tmp;
+    }
+
+  public  Set<String> izGrada(Grad g) {
+
+    }
 
 }
